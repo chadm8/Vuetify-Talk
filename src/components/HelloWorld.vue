97b5e21aa7cd5aca76cfefe6f453592e7c626bad
Chad Martin <martincc@dukes.jmu.edu>
2022-05-21 00:04:15 -0400

    initial setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,57 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container class="d-inline-flex">
+    <v-row class="justify-space-around">
+      <v-card v-for="user in users" :key="user.name" width="400px" class="ma-4">
+        <v-system-bar color="blue darken-2" dark> </v-system-bar>
+        <v-app-bar dark color="blue">
+          <v-avatar color="white">
+            <v-icon large color="red">{{ user.icon }}</v-icon>
+          </v-avatar>
+        </v-app-bar>
+        <v-card-title>{{ user.name }}</v-card-title>
+        <v-card-subtitle>{{ user.title }}</v-card-subtitle>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      users: [
+        {
+          name: "Sarah",
+          title: "Software Engineer",
+          icon: "mdi-airplane",
+        },
+        {
+          name: "Bill",
+          title: "Network Engineer",
+          icon: "mdi-dog",
+        },
+        {
+          name: "Jared",
+          title: "Business Analyst",
+          icon: "mdi-crown",
+        },
+        {
+          name: "Chloe",
+          title: "Software Developer",
+          icon: "mdi-car",
+        },
+        {
+          name: "Jim",
+          title: "Cloud Architect",
+          icon: "mdi-flower",
+        },
+        {
+          name: "Alexa",
+          title: "Coffee Maker",
+          icon: "mdi-cat",
+        },
+      ],
+    };
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
